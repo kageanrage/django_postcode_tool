3e1719c158d53cd022cd9b654e46945f784c84a7
kageanrage <kevinjpickett@gmail.com>
2019-07-19 16:30:34 +1000

    crispy forms implemented

--- pc_project/settings.py
+++ pc_project/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pc_tool'
+    'pc_tool',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
