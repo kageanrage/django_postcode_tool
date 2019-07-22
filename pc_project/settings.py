32d762da14c9420ef0d35925900bceb3842f7649
kageanrage <kevinjpickett@gmail.com>
2019-07-22 15:00:50 +1000

    tweaking key

--- pc_project/settings.py
+++ pc_project/settings.py
@@ -14,7 +14,8 @@ import os
 from decouple import config, Csv
 
 
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'lY3fYCgTQiSE3zR8WrWmbg2BsRHn2odz'
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
